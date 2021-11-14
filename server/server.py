91e11b8cf07be39f9638e0e5850d2ee051637933
HoaiPhan <huyvongmongmanh75@gmail.com>
2021-11-15 00:09:06 +0700

    ddd

---
+++ server/server.py
@@ -0,0 +1,36 @@
+import pyodbc
+import socket
+import threading
+
+hostName=socket.gethostname()
+hostAdd=socket.gethostbyname(hostName)
+print("Host name "+hostName)
+print("Host Add "+hostAdd)
+
+hostPort=int(input('Chon Port muon su dung:'))
+
+
+soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+#Ràng buộc địa chỉ (tên máy chủ, số cổng) vào socket.
+soc.bind((hostAdd,hostPort))
+soc.listen(1)
+
+
+try:
+    clientConnection, clientAdd =soc.accept()
+    print("Client Address: ",clientAdd)
+   
+    while(1):
+        msg=input("username:")
+        msg2=input("password:")
+        clientConnection.send(msg.encode())
+        clientConnection.send(msg2.encode())
+        msg=clientConnection.recv(1024).decode()
+        print("Client: "+msg)
+
+        if(msg=="x"):
+            break
+except: #Bắt trường hợp client đóng kết nối đột ngột
+    print("Error")
+
+clientConnection.close()
