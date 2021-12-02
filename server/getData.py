2177916f4062a51bcb04020b6ac14cf8105baeee
HoaiPhan <huyvongmongmanh75@gmail.com>
2021-12-02 09:55:34 +0700

    ...

--- server/getData.py
+++ server/getData.py
@@ -1,35 +1,51 @@
 import requests, json
-
+import os
 from bs4 import BeautifulSoup
-
-response = requests.get("https://tygia.com/json.php?ran=0&rate=0&gold=1&bank=VIETCOM&date=now")
-
-#soup = BeautifulSoup(response.content, "html.parser")
-
-data = response.content.decode('UTF-8-sig')
-
-#print(data) data kiểu là string
+from datetime import date
 
 
-#chuyển kiểu dữ liệu của data từ string sang dict (có cấu trúc tương tự json)
-js=json.loads(data)
+def getDataFromWeb():
+	response = requests.get("https://tygia.com/json.php?ran=0&rate=0&gold=1&bank=VIETCOM&date=now")
+	#soup = BeautifulSoup(response.content, "html.parser")
 
-#sử dụng phương thức get() của dict lấy ra các giá trị của golds =>js1 lúc này có kiểu list
-js1=js.get('golds')
+	data = response.content.decode('UTF-8-sig')
+	#data kiểu là string
+	#chuyển kiểu dữ liệu của data từ string sang dict (có cấu trúc tương tự json)
+	js=json.loads(data)
 
-#js1[0] => lấy phần đầu của danh sách; js1[0] có kiểu dict =>js1[0].get('value') có kiểu list 
-js2 = js1[0].get('value')
-#print(js2)
+	#sử dụng phương thức get() của dict lấy ra các giá trị của golds =>js1 lúc này có kiểu list
+	js1=js.get('golds')
 
-listValue = []
-for value in js2:
-	listValue.append(value)
-#getdata=json.dumps(listValue,indent=4)
-#print(getdata)
-with open('data.json',mode='w',encoding='UTF-8') as data:
-	json.dump(listValue,data)
+	#js1[0] => lấy phần đầu của danh sách; js1[0] có kiểu dict =>js1[0].get('value') có kiểu list 
+	js2 = js1[0].get('value')
+	return js2
+
+	#listValue = []
+	#for value in js2:
+	#	listValue.append(value)
+	#return listValue
+
+
+#with open('data.json',mode='r',encoding='UTF-8') as data:
+#	getdata=json.load(data)
+#	print(getdata)
+
+
+
+def checkExistFile():
+	today=date.today()
+	d1=today.strftime("%Y%m%d")
+	filepath='./data/'+d1+'.json'
+	if(os.path.isfile(filepath)==False):
+		dataWeb=getDataFromWeb()
+		with open(filepath,mode='w',encoding='UTF-8') as file:
+			#json.dump(dataWeb,file)
+			file.write(str(dataWeb))
+
+checkExistFile()
 
 with open('data.json',mode='r',encoding='UTF-8') as data:
 	getdata=json.load(data)
-	print(getdata)
-input()
+	if(getdata[0]['brand']=='Long Xyên'):
+		print('ok')
+
--- server/test.py
+++ server/test.py
@@ -1,19 +1,13 @@
+from datetime import date
 import json
-import io
+import getData
 
-username="hoaiphanxuan"
-passwork="leomessi"
-print(f"{username}")
-lisUser=[]
-with open('account.json',mode='r+',encoding="utf-8") as acc:
-        data=json.load(acc)
-        for user in data:
-            if(user['username']==username):
-               print('no')
-            else:
-            	lisUser.append(user)
-        dic={"username":f"{username}","pass":f"{passwork}"}
-        lisUser.insert(0,dic)
-        print(lisUser)
-with open('account.json',mode='w',encoding="utf-8") as acc:
-	json.dump(lisUser,acc)
+def checkDataInFile():
+    today=date.today()
+
+    d1=today.strftime("%Y%m%d")
+    print(d1)
+    
+
+#gD.listValue
+checkDataInFile()
